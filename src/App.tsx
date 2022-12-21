f6f3d876c7b9adfe4832ac574420b4b0572a7519
Jeronimo Clinaz <jeronimo.clinaz@cedalio.com>
2022-12-21 12:41:46 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Daily Todo</h1>
+        <p>WalletConnect</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
