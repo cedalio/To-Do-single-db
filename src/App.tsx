811f42173c171721ddb3c8d26ebf2c919785662d
Jeronimo Clinaz <jeronimo.clinaz@cedalio.com>
2022-12-21 15:20:58 -0300

    using modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,42 @@
-import Profile from './components/Profile'
-import { configureChains, WagmiConfig, createClient } from 'wagmi'
 import {
   EthereumClient,
   modalConnectors,
   walletConnectProvider,
 } from "@web3modal/ethereum";
-import { arbitrum, mainnet, polygon } from "wagmi/chains";
-import { Web3Modal } from "@web3modal/react";
-const chains = [arbitrum, mainnet, polygon];
-// Wagmi client
-const projectId = String(process.env.REACT_APP_WC_PROJECT_ID)
-const { provider } = configureChains(chains, [
-  walletConnectProvider({ projectId: projectId }),
-]);
 
-const client = createClient({
+import { Web3Modal } from "@web3modal/react";
+
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+import HomePage from "./HomePage";
+import { useWeb3ModalTheme } from "@web3modal/react";
+import { Web3Button } from "@web3modal/react";
+const chains = [arbitrum, mainnet, polygon];
+
+// Wagmi client
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: "<YOUR_PROJECT_ID>" }),
+]);
+const wagmiClient = createClient({
   autoConnect: true,
   connectors: modalConnectors({ appName: "web3Modal", chains }),
   provider,
 });
 
 // Web3Modal Ethereum Client
-const ethereumClient = new EthereumClient(client, chains);
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 export default function App() {
+  const { theme, setTheme } = useWeb3ModalTheme();
   return (
     <>
-      <WagmiConfig client={client}>
-        <Profile />
+      <WagmiConfig client={wagmiClient}>
+        <HomePage />
       </WagmiConfig>
-
+      <Web3Button />
       <Web3Modal
-        projectId={projectId}
+        projectId="0b5c910d4315b61602f1679c78ea6922"
         ethereumClient={ethereumClient}
       />
     </>
---
+++ src/HomePage.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <>
+            <h1>Daily Todo</h1>
+        </>
+
+    )
+}
