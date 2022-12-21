b874374dee933f7b4301a27779fd6a2958f2f114
Jeronimo Clinaz <jeronimo.clinaz@cedalio.com>
2022-12-21 14:40:56 -0300

    WIP web3modal configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
-import './App.css';
+import Profile from './components/Profile'
+import { configureChains, WagmiConfig, createClient } from 'wagmi'
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+import { Web3Modal } from "@web3modal/react";
+const chains = [arbitrum, mainnet, polygon];
+// Wagmi client
+const projectId = String(process.env.REACT_APP_WC_PROJECT_ID)
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: projectId }),
+]);
 
-function App() {
+const client = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: "web3Modal", chains }),
+  provider,
+});
+
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(client, chains);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 >Daily Todo</h1>
-      </header>
-      <p>WalletConnect</p>
-    </div>
+    <>
+      <WagmiConfig client={client}>
+        <Profile />
+      </WagmiConfig>
+
+      <Web3Modal
+        projectId={projectId}
+        ethereumClient={ethereumClient}
+      />
+    </>
   );
 }
-
-export default App;
