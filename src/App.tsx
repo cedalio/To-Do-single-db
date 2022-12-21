768fee2e56b4eafacc332dcb05b9c34e4fa7ebbd
Jeronimo Clinaz <jeronimo.clinaz@cedalio.com>
2022-12-21 12:55:18 -0300

    Initial setup and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Daily Todo</h1>
-        <p>WalletConnect</p>
+        <h1 >Daily Todo</h1>
       </header>
+      <p>WalletConnect</p>
     </div>
   );
 }
