3382b3f0e6bd3d428ea7d6996998f2cd49edee76
Jeronimo Clinaz <jeronimo.clinaz@cedalio.com>
2022-12-30 15:52:33 -0300

    filtering todos by owner

--- src/components/ListComponent.tsx
+++ src/components/ListComponent.tsx
@@ -13,7 +13,8 @@ type Todo = {
     description?: string,
     tags: Array<string>,
     priority: number,
-    id: string
+    id: string,
+    owner: string
 }
 
 // const todosTest: Array<Todo> = [
@@ -89,6 +90,7 @@ const GET_TODOS = gql`
             title
             description
             priority
+            owner
         }
   }
 `;
@@ -97,37 +99,45 @@ const GET_TODOS = gql`
 export default function ListComponent(props: { address: string | undefined }) {
     const [todos, setTodos] = React.useState<Todo[]>([]);
     const [newTodo, setNewTodo] = React.useState<Todo>();
-
+    
+    const ownerAddress:string = String(props.address)
 
     const defaultTodo = {
         title: "This Is Your First ToDo Card!",
         description: "Buy some food for my dog and change their water",
         tags: ["healt", "rutine"],
         priority: 1,
-        id: "abcdefg12345"
+        id: "abcdefg12345",
+        owner: "abcdefg123"
     }
 
     function DisplayCard(props: { todo: Todo }) {
-        return (
-            <Card sx={{ minWidth: "500px", maxWidth: "60%", mb: 3, borderRadius: "11px", boxShadow: "0px 2px 1px -1px rgb(0 0 0 / 0%), 0px 1px 1px 0px rgb(0 0 0 / 7%), 0px 1px 3px 0px rgb(0 0 0 / 3%)" }}>
-                <CardContent>
-                    <Typography sx={{ fontSize: 23, fontWeight: 600, textAlign: "justify" }} color="black" gutterBottom>
-                        {props.todo.title}
-                    </Typography>
-                    <Typography sx={{ fontSize: 14, textAlign: "justify" }} color="text.secondary" gutterBottom>
-                        {props.todo.description}
-                    </Typography>
-                    <Stack spacing={1} alignItems="left">
-                        <Stack direction="row" spacing={1}>
-                            <Chip color="error" label={`P${props.todo.priority}`} sx={{ fontWeight: "600", backgroundColor: "hsl(0deg 86% 97%)", color: "hsl(347deg 77% 56%)" }}></Chip>
-                            {/* {props.todo.tags.map((tag) => {
-                                return <Chip label={tag} key={tag} color="success" sx={{ fontWeight: "600", backgroundColor: "hsl(138deg 76% 97%)", color: "hsl(142deg 61% 42%)" }} />
-                            })} */}
+
+        if (props.todo.owner === ownerAddress) {
+            return (
+                <Card sx={{ minWidth: "500px", maxWidth: "60%", mb: 3, borderRadius: "11px", boxShadow: "0px 2px 1px -1px rgb(0 0 0 / 0%), 0px 1px 1px 0px rgb(0 0 0 / 7%), 0px 1px 3px 0px rgb(0 0 0 / 3%)" }}>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 23, fontWeight: 600, textAlign: "justify" }} color="black" gutterBottom>
+                            {props.todo.title}
+                        </Typography>
+                        <Typography sx={{ fontSize: 14, textAlign: "justify" }} color="text.secondary" gutterBottom>
+                            {props.todo.description}
+                        </Typography>
+                        <Stack spacing={1} alignItems="left">
+                            <Stack direction="row" spacing={1}>
+                                <Chip color="error" label={`P${props.todo.priority}`} sx={{ fontWeight: "600", backgroundColor: "hsl(0deg 86% 97%)", color: "hsl(347deg 77% 56%)" }}></Chip>
+                                {/* {props.todo.tags.map((tag) => {
+                                    return <Chip label={tag} key={tag} color="success" sx={{ fontWeight: "600", backgroundColor: "hsl(138deg 76% 97%)", color: "hsl(142deg 61% 42%)" }} />
+                                })} */}
+                            </Stack>
                         </Stack>
-                    </Stack>
-                </CardContent>
-            </Card>
-        )
+                    </CardContent>
+                </Card>
+            )
+        }
+        else{
+            return <></>
+        }
     }
 
     const { loading, error, data } = useQuery(GET_TODOS);
@@ -164,7 +174,7 @@ export default function ListComponent(props: { address: string | undefined }) {
         return (
             <div className="list-container">
                 {displayTodos()}
-                <TodoInputComponent setState={setNewTodo} address={props.address}/>
+                <TodoInputComponent setState={setNewTodo} address={ownerAddress} />
             </div>
         );
     }
