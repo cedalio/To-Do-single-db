0a4844642b3dca63ab196c3ef463139b320cc84a
Jeronimo Clinaz <jeronimo.clinaz@cedalio.com>
2023-01-19 12:19:47 -0300

    Drag and drop color effects

--- src/components/ListComponent.tsx
+++ src/components/ListComponent.tsx
@@ -5,6 +5,8 @@ import TodoInputComponent from "./TodoInputComponent"
 import { DragDropContext, Droppable } from "react-beautiful-dnd"
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
     props,
@@ -43,7 +45,12 @@ const GET_TODOS = gql`
   }
 `;
 
-
+const dragColors = {
+    initialColor: "#63636324",
+    dragStartColor:"#636363",
+    doneColor:"#6ab05a",
+    deleteColor:"#ea3b3b"
+}
 
 export default function ListComponent(props: { address: string | undefined }) {
     const [todos, setTodos] = React.useState<Todo[]>([]);
@@ -51,6 +58,8 @@ export default function ListComponent(props: { address: string | undefined }) {
     const [update, setUpdate] = React.useState<Update>();
     const ownerAddress: string = String(props.address)
     const [open, setOpen] = React.useState(false);
+    const [doneIconColor, setDoneIconColor] = React.useState(dragColors.initialColor);
+    const [deleteIconColor, setDeleteIconColor] = React.useState(dragColors.initialColor);
 
 
     const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
@@ -110,16 +119,35 @@ export default function ListComponent(props: { address: string | undefined }) {
     }
 
     function onDragEnd(result: any) {
+        setDeleteIconColor(dragColors.initialColor)
+        setDoneIconColor(dragColors.initialColor)
         setUpdate({ update: result.destination.droppableId, todoId: result.draggableId })
+    }
+
+    function onDragStart(result:any){
+        setDeleteIconColor(dragColors.dragStartColor)
+        setDoneIconColor(dragColors.dragStartColor)
+    }
+    function onDragUpdate(result:any){
+        console.log(result.destination.droppableId)
+        if(result.destination.droppableId === "delete"){
+            setDeleteIconColor(dragColors.deleteColor)
+        }else if(result.destination.droppableId === "done"){
+            setDoneIconColor(dragColors.doneColor)
+        }else{
+            setDeleteIconColor(dragColors.dragStartColor)
+            setDoneIconColor(dragColors.dragStartColor)
+        }
     }
 
     if (props.address) {
         return (
-            <DragDropContext onDragEnd={onDragEnd}>
+            <DragDropContext onDragEnd={onDragEnd} onDragStart={onDragStart} onDragUpdate={onDragUpdate}>
                 <div className="container">
                     <Droppable droppableId='delete'>
                         {(provided) => (
                             <div {...provided.droppableProps} ref={provided.innerRef} className="delete-container">
+                                <HighlightOffIcon fontSize="large" sx={{height: "200px", width:"200px", color:deleteIconColor }}/>
                                 {provided.placeholder}
                             </div>
                         )}
@@ -136,6 +164,7 @@ export default function ListComponent(props: { address: string | undefined }) {
                     <Droppable droppableId='done'>
                         {(provided) => (
                             <div {...provided.droppableProps} ref={provided.innerRef} className="done-container">
+                                <CheckCircleOutlineIcon fontSize="large" sx={{height: "200px", width:"200px", color:doneIconColor }}/>
                                 {provided.placeholder}
                             </div>
                         )}
