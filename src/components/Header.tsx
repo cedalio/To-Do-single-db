cf443807da3c655f86467eebb34e7595d0ff700e
Jeronimo Clinaz <jeronimo.clinaz@cedalio.com>
2023-02-01 18:04:48 -0300

    powered by + favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,9 @@ export default function Header() {
     return (
         <div className="App-header">
             <h1 className="title">Daily Todo</h1>
+            <div className="powered-by">
+                Powered By <img src="cedalio-icon.png" alt="" />
+            </div>
         </div>
 
     )
