93962b2bb479326550e6416c0e9fd728340ec17a
Jeronimo Clinaz <jeronimo.clinaz@cedalio.com>
2023-02-02 12:44:27 -0300

    footer completed (structure +media)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,36 @@
 export default function Footer() {
     return (
-        <div className="App-footer">
-            <div className="line">
-            </div>
-            <div className="texts">
-                <p>This is a example ToDo app that show the power of Cedalio's technology. This is only a demo app using a Testnet. All the information here is stored in a smart contract. If you want to learn more please clic here. Or if you have some feedback please feel free to reach out us!</p>
-                <div className="address">
-                    <p>Smart Contract Address:   0x2f5c73ef5675tr12du45sDFd...</p>
-                    <img src="polygon.png" alt="polygon icon" />
+        <footer>
+            <div className="App-footer">
+                <div className="line">
+                </div>
+                <div className="texts">
+                    <p>This is a example ToDo app that show the power of Cedalio's technology. This is only a demo app using a Testnet. <strong>All the information here is stored in a smart contract</strong>. If you want to learn more please clic here. Or if you have some feedback please feel free to reach out us!</p>
+                    <div className="address">
+                        <p>Smart Contract Address:   0x2f5c73ef5675tr12du45sDFd...</p>
+                        <img src="polygon.png" alt="polygon icon" />
+                    </div>
+                </div>
+                <div className="branding">
+                    <div className="logo">
+                        <img src="favicon-32x32.png" alt="cedalio" />
+                        <h2>DECENTRALIZED
+                            TODO DAPP</h2>
+                    </div>
+                    <div className="rights">
+                        <img src="cedalio-icon.png" alt="cedalio" /> 2023 All Rights Reserved.
+                    </div>
+                    <div className="social">
+                        <img src="discord.png" alt="discord" />
+                        <img src="github.png" alt="github" />
+                        <img src="linkedin.png" alt="linkedin" />
+                        <img src="medium.png" alt="medium" />
+                        <img src="twitter.png" alt="twitter" />
+                        <img src="youtube.png" alt="youtube" />
+                    </div>
                 </div>
             </div>
-            <div className="branding">
-
-            </div>
-        </div>
+        </footer>
 
     )
 }
