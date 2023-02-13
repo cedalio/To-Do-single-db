245519e7f0709d9fb992b4929ff4ff7144acec76
Jeronimo Clinaz <jeronimo.clinaz@cedalio.com>
2023-02-13 10:01:30 -0300

    using environment variable

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { HttpLink } from '@apollo/client';
 
 const link = new HttpLink({
-  uri: `https://gateway.staging.cedalio.io/${String(process.env.REACT_APP_DEPLOYMENT_ID)}/graphql`,
+  uri: `${String(process.env.REACT_APP_GRAPHQL_GATEWAY_BASE_URL)}/${String(process.env.REACT_APP_DEPLOYMENT_ID)}/graphql`,
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json"
